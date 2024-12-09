0b47db6f181b6b55f0045d75c346b569887835a7
renz <rdioneda4@outlook.com>
2024-12-09 21:11:58 +0800

    signup

--- Home.php
+++ Home.php
@@ -19,76 +19,44 @@
 <body>
 
   <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-black">
-  <div class="container">
-    <!-- Burger Menu (Left-Aligned) -->
-    <button class="navbar-toggler me-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileMenu" aria-controls="mobileMenu">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <!-- Logo -->
-    <a class="navbar-brand" href="Home.php">
-      <img src="images/Logo.png" alt="Junk Food Logo" class="navbar-logo">
-    </a>
-
-    <!-- Main Menu (Desktop View) -->
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav mx-auto">
-        <li class="nav-item">
-          <a class="nav-link text-white active" href="Home.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="Shop.php">Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="Contact.php">Contact</a>
-        </li>
-      </ul>
-      <div class="d-flex align-items-center">
-        <a href="#" class="text-white me-3">
-          <i class="fas fa-search fa-lg"></i>
-        </a>
-        <a href="#" class="text-white me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
-          <i class="fas fa-user fa-lg"></i>
-        </a>
-        <a href="Cart.php" class="text-white">
-          <i class="fas fa-shopping-bag fa-lg"></i>
-        </a>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+    <div class="container">
+      <!-- Logo -->
+      <a class="navbar-brand" href="Home.php">
+        <img src="images/Logo.png" alt="Junk Food Logo" class="navbar-logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <!-- Centered Links -->
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link text-white active" href="Home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="Shop.php">Shop</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="Contact.php">Contact</a>
+          </li>
+        </ul>
+        <!-- Right-aligned Icons -->
+        <div class="d-flex align-items-center">
+          <a href="#" class="text-white me-3">
+            <i class="fas fa-search fa-lg"></i>
+          </a>
+          <div class="d-flex align-items-center">
+            <a href="#" class="text-white me-3" data-bs-toggle="modal" data-bs-target="#loginModal">
+              <i class="fas fa-user fa-lg"></i>
+            </a>
+            <a href="Cart.php" class="text-white">
+              <i class="fas fa-shopping-bag fa-lg"></i>
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
-
-<!-- Offcanvas Menu (Mobile View) -->
-<div class="offcanvas offcanvas-start bg-black" tabindex="-1" id="mobileMenu" aria-labelledby="mobileMenuLabel">
-  <div class="offcanvas-header">
-    <!-- Logo in Offcanvas -->
-    <a class="navbar-brand" href="Home.php">
-      <img src="images/Logo.png" alt="Junk Food Logo" class="navbar-logo">
-    </a>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link text-white active" href="Home.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-white" href="Shop.php">Shop</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-white" href="Contact.php">Contact</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-white" data-bs-toggle="modal" data-bs-target="#loginModal">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-white" href="Cart.php">Cart</a>
-      </li>
-    </ul>
-  </div>
-</div>
-
+  </nav>
 
   <!-- Login Modal -->
   <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
@@ -145,7 +113,7 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <form id="signupForm" action="register.php" method="POST">
+          <form action="register.php" method="POST">
             <!-- Username -->
             <div class="form-group">
               <i class="fas fa-user"></i>
