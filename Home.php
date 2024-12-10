12205cc51673f2deddac763ca6c187d25e669126
renz <rdioneda4@outlook.com>
2024-12-10 11:27:50 +0800

    login

--- Home.php
+++ Home.php
@@ -462,7 +462,6 @@
   <!-- Bootstrap JS -->
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="signup.js"></script>
 </body>
 
 </html>
